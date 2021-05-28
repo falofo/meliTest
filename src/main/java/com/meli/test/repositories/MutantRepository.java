04b8a1771cdcca5e23bd9e1039c88d17c284fa2b
Fabiani Lozano <falofo23@gmail.com>
2021-05-28 08:33:09 -0500

    Se cambia persistencia de Dynamo por H2

--- src/main/java/com/meli/test/repositories/MutantRepository.java
+++ src/main/java/com/meli/test/repositories/MutantRepository.java
@@ -1,13 +1,11 @@
 package com.meli.test.repositories;
 
 import com.meli.test.models.entity.MutantEntity;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScanCount;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
-@EnableScan
-@EnableScanCount
+@Repository
 public interface MutantRepository extends CrudRepository<MutantEntity, String> {
 
     long countByMutant(boolean mutant);
